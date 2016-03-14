0d38170c75baed3a6294c77d4785184afec6d71e
Siva Chintakana <siva@cliqr.com>
2016-03-14 14:41:04 -0700

    added logic to take the range limit value as an argument

--- bizzbuzz.py
+++ bizzbuzz.py
@@ -2,7 +2,9 @@
 
 # Python exercise based on https://github.com/jasonkeene/mosi-python-workshop/blob/master/0045_exercise_bizzbuzz.ipynb
 
-for i in range(1,200):
+import sys
+
+for i in range(1,int(sys.argv[1])):
     if i%3 == 0 and i%5 == 0:
         print "BizzBuzz"
     elif i%3 == 0:
