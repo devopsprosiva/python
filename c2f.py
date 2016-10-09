64b52d5f0cdd5f744c9ee3e21299c70ba7466570
DevOpsPro <devopsprosiva@gmail.com>
2016-10-09 01:06:17 -0700

    adding condition for C temp lower than -273.15

--- c2f.py
+++ c2f.py
@@ -5,7 +5,10 @@ input_temp=int(raw_input("Enter the temperature in Celsius: "))
 #cel_temp=32
 
 def c2f(cel_temp=input_temp):
-    fah_temp=(cel_temp*1.8)+32
-    return fah_temp
+    if cel_temp < -273.15:
+        return "The lowest possible temperature that physical matter can reach is -273.15C"
+    else:
+        fah_temp=(cel_temp*1.8)+32
+        return fah_temp
 
 print(c2f(input_temp))
