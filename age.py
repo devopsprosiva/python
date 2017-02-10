41d2cdc1a5567e700d89391835a8122188ba6dab
DevOpsPro <devopsprosiva@gmail.com>
2017-02-10 15:14:19 -0800

    Updated code to print the number of years left to reach age of 100

--- age.py
+++ age.py
@@ -1,13 +1,25 @@
 #!/usr/local/bin/python
 
+#################################################################################################################
+# Create a program that asks the user to enter their name and their age.                                        #
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.             #
+#################################################################################################################
+
 import sys
 
 # input function in Python 2.7, evaluates whatever your enter, as a Python expression.
 # If you simply want to read strings, then use raw_input function in Python 2.7, which will not evaluate the read strings.
 # If you are using Python 3.x, raw_input has been renamed to input
 
+# Using raw_input for string
 name = raw_input("Enter your name: ")
-age = raw_input("Enter your age: ")
 
-print("Your name is " + name)
-print("Your age is " + age)
+# Using input for integers as it will automatically interpret as number
+age = input("Enter your age: ")
+
+# Calculate the number of years remaining to reach 100 years
+years_to_hundred = 100 - age
+
+# Since a string and an integer cannot be concatenated in the same print statement,
+# converting years_to_hundred to string using the str function
+print(name + ", you will be hundred in " + str(years_to_hundred) + " years")
