3121bbd2b5c62709b851aa94367e5f578f1f39cc
DevOpsPro <devopsprosiva@gmail.com>
2017-02-10 16:39:23 -0800

    Added logic to print the year in which you turn 100

--- age.py
+++ age.py
@@ -6,7 +6,7 @@
 #################################################################################################################
 
 import sys
-
+import datetime
 # input function in Python 2.7, evaluates whatever your enter, as a Python expression.
 # If you simply want to read strings, then use raw_input function in Python 2.7, which will not evaluate the read strings.
 # If you are using Python 3.x, raw_input has been renamed to input
@@ -17,8 +17,14 @@ name = raw_input("Enter your name: ")
 # Using input for integers as it will automatically interpret as number
 age = input("Enter your age: ")
 
+# Get the current year
+current_year = datetime.datetime.now().year
+
 # Calculate the number of years remaining to reach 100 years
 years_to_hundred = 100 - age
+
+# Calculate the year in which you will be 100
+hundredth_year = current_year + years_to_hundred
 
 # To assign a value to a variable, use '='
 # To compare values of two variables/operands, use '=='
@@ -28,4 +34,4 @@ if years_to_hundred == 0:
 else:
     # Since a string and an integer cannot be concatenated in the same print statement,
     # converting years_to_hundred to string using the str function
-    print(name.capitalize() + ", you will be hundred in " + str(years_to_hundred) + " years")
+    print(name.capitalize() + ", you will be hundred in " + str(years_to_hundred) + " years in the year " + str(hundredth_year) + "!!!")
