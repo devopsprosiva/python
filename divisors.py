85f95558edb1aee86641203724d6eb1e4a202b78
DevOpsPro <devopsprosiva@gmail.com>
2017-02-14 14:11:52 -0800

    added source link

--- divisors.py
+++ divisors.py
@@ -2,6 +2,7 @@
 
 #################################################################################################################
 # Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+# http://www.practicepython.org/exercise/2014/02/26/04-divisors.html
 #################################################################################################################
 
 
